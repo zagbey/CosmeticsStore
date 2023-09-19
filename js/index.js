5eb6b46d9c12f35cfad94c36504af3c0a884cd72
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-09-19 13:24:09 +0300

    çok karıştı buralar

--- js/index.js
+++ js/index.js
@@ -66,3 +66,17 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 //! slider end
+
+//! ////////////////////////////////////////////////////////////
+
+//! add product to local storage 
+
+async function getData(){
+    const photos = await fetch("../js/data.json");
+    const data = await photos.json();
+
+    data ? localStorage.setItem("products", JSON.stringify(data)) : [];
+}
+getData();
+
+const products = localStorage.getItem("products")
