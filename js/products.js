3dd402c1dddd1497a7a9a7a653938b85ceb20c48
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-09-20 13:12:42 +0300

    fix

--- js/products.js
+++ js/products.js
@@ -1,6 +1,6 @@
 function productsFunc(){
     const products = JSON.parse(localStorage.getItem("products"));
-
+    //her bir ürün 
     products.array.forEach(item => {
         console.log(item.name);
     });
